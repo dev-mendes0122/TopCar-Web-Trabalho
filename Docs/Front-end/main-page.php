0a7b4fbbcd7c129d3a907efd705ccbd2a0c49ff1
Fellipe Mendes <fellipemendes012@gmail.com>
2025-06-10 19:27:59 -0300

    BD ok

--- Docs/Front-end/main-page.php
+++ Docs/Front-end/main-page.php
@@ -1,3 +1,11 @@
+<?php
+session_start();
+if (!isset($_SESSION['usuario'])) {
+    header("Location: login.html");
+    exit();
+}
+?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
@@ -7,10 +15,7 @@
     <link rel="icon" href="assets/site-icon.svg">
     <link rel="stylesheet" href="Styles/style-main-page.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
-  </head>
+</head>
 <body>
   <header class="flex">
     <nav class="flex nav-links-right">
@@ -45,7 +50,7 @@
             <a class="nav-link" href="supercarros.html">Super Carros</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="anuncie.html">Anuncie Aqui</a>
+            <a class="nav-link" href="#">Anuncie Aqui</a>
           </li>
         </ul>
       </div>
@@ -53,8 +58,8 @@
   </nav>
     <main class="flex">
         <div class="flex main-ad">
-            <img class="main-car" src="assets/carros/camaro transparent.avif" alt="">
-            <a class="botao" href="carro.html?id=camaro">ADQUIRIR</a>
+            <img class="main-car" src="assets/camaro transparent.avif" alt="">
+            <a class="botao" href="carro.html">ADQUIRIR</a>
             <div class="flex highlights-space">
                 <div class="flex highlights">
                     <p>MOTOR 6.2L V8</p>
